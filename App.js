a72b4349068602d2d3ef8ccd99844729a3ea0028
korayerdem89 <developerkoray@gmail.com>
2022-10-24 10:23:01 +0300

    inputContainer tasarımına devam

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 
 import { StyleSheet } from 'react-native';
+
 import StartGameScreen from './screens/StartGameScreen';
 
-
 export default function App() {
-  return 
-  <StartGameScreen />;
+  return <StartGameScreen />
 }
 
 const styles = StyleSheet.create({
