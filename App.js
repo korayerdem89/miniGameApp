98f13ee5c081db4fa0909d021a6817c7da6bade6
korayerdem89 <developerkoray@gmail.com>
2022-10-24 12:32:30 +0300

    root background ayarlandı

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient} from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329','#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground source={require('./assets/images/dices.jpg')} resizeMode='cover' style={styles.rootScreen} imageStyle={styles.backgroundImage}>
       <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   )
 }
@@ -13,5 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen:{
     flex:1,
+  },
+  backgroundImage:{
+    opacity: 0.15
   }
 });
